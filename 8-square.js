035e194b7c2d9dd978dee3f507832b436016e37c
vincidax <vincentdx18@gmail.com>
2025-11-14 06:05:31 +0200

    updating 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -1,23 +1,22 @@
-argv = process.argv;
-myNumber = parseInt(argv[2])
-if (!Number.isNaN(parseInt(argv[2]))){
+const argv = process.argv;
+const myNumber = parseInt(argv[2]);
+
+if (!Number.isNaN(myNumber)) {
     if (myNumber <= 0) {
         
-    }
-    else if (myNumber > 0){
-        for (i= 0; i < myNumber; i++){
-            console.log(repeatCharacter('x', myNumber))
+    } else if (myNumber > 0) {
+        for (let i = 0; i < myNumber; i++) {
+            console.log(repeatCharacter('x', myNumber));
         }
     }
-}
-else {
-    console.log("Missing size")
+} else {
+    console.log("Missing size");
 }
 
 function repeatCharacter(char, count) {
-  let result = '';
-  for (let i = 0; i < count; i++) {
-    result += char;
-  }
-  return result;
+    let result = '';
+    for (let i = 0; i < count; i++) {
+        result += char;
+    }
+    return result;
 }
